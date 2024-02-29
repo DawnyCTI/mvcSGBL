5c531db57f9799ffb62fdc06f8c02a2901e79065
Dawny <20221014@itla.edu.do>
2024-02-29 13:43:06 -0400

    Aqui cambie la solucion para trabajar con la capa presentacion

--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -1,32 +1,23 @@
-using CapaPresentacionAdmin.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace CapaPresentacionAdmin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult Usuarios()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
+
 }
---
+++ CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        // GET: Mantenedor
+        public ActionResult Generos()
+        {
+            return View();
+        }
+        public ActionResult Prestamos()
+        {
+            return View();
+        }
+        public ActionResult Libros()
+        {
+            return View();
+        }
+        public ActionResult Reservas()
+        {
+            return View();
+        }
+    }
+}
